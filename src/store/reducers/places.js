14aa06a994a6c22c2b2fca026dd9ed0d9f398d31
Ali Havasi <alihvs@gmail.com>
2018-06-20 17:48:47 +0430

    after adding navigation

---
+++ src/store/reducers/places.js
@@ -0,0 +1,34 @@
+import { ADD_PLACE, DELETE_PLACE } from "../actions/actionTypes";
+
+const initialState = {
+  places: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri:
+              "http://www.attractivepartners.co.uk/wp-content/uploads/2017/06/profile.jpg"
+          }
+        })
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => {
+          return place.key !== action.placeKey;
+        }),
+        selectedPlace: null
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
